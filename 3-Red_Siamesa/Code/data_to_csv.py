235ef2208990d4f7043dd496b0467197d93c1763
jacobogdefrutos <jacobo15defrutos@gmail.com>
2023-10-16 12:15:16 +0200

    New Data

--- 3-Red_Siamesa/Code/data_to_csv.py
+++ 3-Red_Siamesa/Code/data_to_csv.py
@@ -2,7 +2,7 @@ import os
 import csv
 #------------Cuando se runee el codigo, se debe de borrar del csv resultante las ultimas lineas hasta la 120 (longitud de la lista)
 def main():
-    directory = r'Data\saved_test_clas_images\val'  # Replace with your directory path
+    directory = r'Data\saved_seg_class_images\val'  # Replace with your directory path
     csv_file_name = 'Iris_val_seg_list.csv'
     file_train_list = os.listdir(directory)
     #Lo dividimos en tres listas: OD, OI y Label
@@ -13,23 +13,31 @@ def main():
         csv_writer = csv.writer(csv_file)
         for idx,name in enumerate(file_OD_list):
             label=101# en case de que haya algun error
-            label2=101
+            label_OI=101
+            label_OD=101
             if 'SANO' in name and 'SANO' in file_OI_list[idx]:
                 label=0
-                label2=0#si el paciente es sano, es decir ambos iris son"iguales"--> label=0
+                label_OI=0
+                label_OD=0#si el paciente es sano, es decir ambos iris son"iguales"--> label=0
             elif ('SANO' in name and 'CMV' in file_OI_list[idx]) or ('CMV' in name and 'SANO' in file_OI_list[idx]):
                 label=1
-                label2=1
             elif ('SANO' in name and 'SURV' in file_OI_list[idx]) or ('SURV' in name and 'SANO' in file_OI_list[idx]):
                 label=2
-                label2=1
             elif ('SANO' in name and 'POSTNER' in file_OI_list[idx]) or ('POSTNER' in name and 'SANO' in file_OI_list[idx]):
                 label=3
-                label2=1
             elif ('SANO' in name and 'SUF' in file_OI_list[idx]) or ('SUF' in name and 'SANO' in file_OI_list[idx]):
                 label=4
-                label2=1
-            csv_writer.writerow([file_OI_list[idx], name, label,label2])
+
+            if 'SANO' not in file_OI_list[idx] and 'SANO' not in name:
+                label_OI=1
+                label_OD=1
+            elif 'SANO'  in file_OI_list[idx] and 'SANO' not in name:
+                label_OI=0
+                label_OD=1
+            elif 'SANO' not in file_OI_list[idx] and 'SANO'  in name:
+                label_OI=1
+                label_OD=0
+            csv_writer.writerow([file_OI_list[idx], name, label,label_OI,label_OD])
 
 if __name__ == "__main__":
     main() 
