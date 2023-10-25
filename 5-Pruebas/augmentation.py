cd374f6ef62b3f6200c2ec54b4cd14909797759e
jacobogdefrutos <jacobo15defrutos@gmail.com>
2023-10-25 16:21:34 +0000

    probando machine learning things

---
+++ 5-Pruebas/augmentation.py
@@ -0,0 +1,39 @@
+import os
+import cv2
+import random
+
+def augment_image(image):
+    # Randomly select the augmentation to apply
+    augmentation = random.randint(0, 1)
+    if augmentation == 0:
+        # Rotate the image by a random angle between -30 and 30 degrees
+        angle = random.randint(-30, 30)
+        height, width = image.shape[:2]
+        # Calculate the rotation matrix
+        rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1)
+        # Apply the rotation to the image
+        image = cv2.warpAffine(image, rotation_matrix, (width, height))
+        #image = cv2.rotate(image, angle)
+        #label= cv2.rotate(label, angle)
+    elif augmentation == 1:
+        # Flip the image horizontally
+        image = cv2.flip(image, 1)
+    return image, augmentation
+
+TRAIN_IMG_DIR='5-Pruebas/Total_images'
+OUTPUT_IMG_DIR= '5-Pruebas/Total_images'
+def main():
+    file_img_list = os.listdir(TRAIN_IMG_DIR)
+    for img in file_img_list:
+        # Read the image
+        image = cv2.imread(os.path.join(TRAIN_IMG_DIR, img))
+        if image  is not None and 'SANO' not in img:
+            augmented_image,i  = augment_image(image)
+            name = img.split(".png")[0]
+            output_img_path = os.path.join(OUTPUT_IMG_DIR, f"{name}_aug.png")
+            cv2.imwrite(output_img_path, augmented_image)
+
+
+    print("End of main")
+if __name__ == "__main__":
+    main()
--- 5-Pruebas/data_to_csv.py
+++ 5-Pruebas/data_to_csv.py
@@ -2,23 +2,26 @@ import os
 import csv
 
 def main():
-    directory = r'Data/saved_seg_class_images/test'  # Replace with your directory path
-    csv_file_name = r'4-Red_Siamesa/Iris_test_basic_seg_list.csv'
+    directory = r'5-Pruebas/test'  # Replace with your directory path
+    csv_file_name = r'5-Pruebas/test.csv'
     file_train_list = os.listdir(directory)
     #Lo dividimos en tres listas: OD, OI y Label
     file_OD_list= [name for name in file_train_list if 'OD' in name ]
     file_OI_list= [name for name in file_train_list if 'OI' in name ]
     file_list= file_OD_list+file_OI_list
-    # cuatro clases: 0 SANO, 1 CMV, 2 SURV, 3 POSTNER, 4 SUF
+    # cuatro clases: 0 SANO, 1 CMV, 2 SURV
     with open(csv_file_name, 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
         for idx,name in enumerate(file_list):
-            label=101# en case de que haya algun error
             if 'SANO'  in name:
                 label=0
-            else:
+                csv_writer.writerow( [name, label])
+            elif 'CMV'in name:
                 label=1
-            csv_writer.writerow( [name, label])
+                csv_writer.writerow( [name, label])
+            elif 'SURV'in name:
+                label=2
+                csv_writer.writerow( [name, label])
 
 if __name__ == "__main__":
     main() 
