31e50b5a0b6f9f94004f442d48d7722c43aa46d6
jacobogdefrutos <jacobo15defrutos@gmail.com>
2023-11-01 10:33:44 +0000

    Probando RS con fft

--- 4-Red_Siamesa/Code/data_to_csv.py
+++ 4-Red_Siamesa/Code/data_to_csv.py
@@ -2,23 +2,26 @@ import os
 import csv
 
 def main():
-    directory = r'Data/saved_seg_class_images/test'  # Replace with your directory path
-    csv_file_name = r'4-Red_Siamesa/Iris_test_basic_seg_list.csv'
+    directory = r'5-Pruebas/Total_images'  # Replace with your directory path
+    csv_file_name = r'4-Red_Siamesa/Code/total_images_RS.csv'
     file_train_list = os.listdir(directory)
     #Lo dividimos en tres listas: OD, OI y Label
     file_OD_list= [name for name in file_train_list if 'OD' in name ]
     file_OI_list= [name for name in file_train_list if 'OI' in name ]
-    file_list= file_OD_list+file_OI_list
     # cuatro clases: 0 SANO, 1 CMV, 2 SURV, 3 POSTNER, 4 SUF
     with open(csv_file_name, 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
-        for idx,name in enumerate(file_list):
-            label=101# en case de que haya algun error
-            if 'SANO'  in name:
+        for idx,name in enumerate(file_OD_list):
+            label=101
+            if 'SANO' in name and 'SANO' in file_OI_list[idx]:
                 label=0
-            else:
+                csv_writer.writerow([file_OI_list[idx], name, label])
+            elif ('SANO' in name and 'CMV' in file_OI_list[idx]) or ('CMV' in name and 'SANO' in file_OI_list[idx]):
                 label=1
-            csv_writer.writerow( [name, label])
+                csv_writer.writerow([file_OI_list[idx], name, label])
+            elif ('SANO' in name and 'SURV' in file_OI_list[idx]) or ('SURV' in name and 'SANO' in file_OI_list[idx]):
+                label=2
+                csv_writer.writerow([file_OI_list[idx], name, label])
 
 if __name__ == "__main__":
     main() 
